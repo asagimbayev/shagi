4dd9447276d7e075939ea36c8e6253aa8136ae52
Anuar <sagimbayev01@gmail.com>
2024-11-08 17:28:28 +0500

    Step 13

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
 
 const App = () => {
 
     const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Implement React Application', important: true },
-        { label: 'Make notes from your stud', important: false },
-        { label: 'Do not drink alchohol', important: false },
+        { label: 'Drink Coffee', important: false,  id: 1},
+        { label: 'Implement React Application', important: true, id: 2 },
+        { label: 'Make notes from your stud', important: false, id: 3 },
+        { label: 'Do not drink alchohol', important: false, id: 4 },
     ];
 
     return (
@@ -18,6 +20,7 @@ const App = () => {
             <AppHeader />
             <SearchPanel />
             <TodoList todos =  {todoData} />
+            <ItemStatusFilter />
         </div>
     );
 }
